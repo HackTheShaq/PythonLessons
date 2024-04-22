eb10654272e950324413209fa27952edeca3ed46
HackTheShaq <s.sunyaykin@gmail.com>
2024-04-22 21:13:42 +0300

    myfirstcommit

---
+++ lesson1.py
@@ -0,0 +1,41 @@
+# a = 10
+# print (a)
+# b = "10"
+# print(type(b))#рядок*
+# print(type(a))#ціле число*
+# """Text
+
+# """
+
+# c = 2.0
+# d = True
+# e = False
+# f = None
+# print(type(c))
+# print(type(d))
+# print(type(e))
+# print(type(f))
+# a = True
+# while a:
+#     print("hello, Serhii")
+#     a = False
+# print(True<False)
+# a:int = 10
+# print(a)
+# mylist:list[int] = [1,2,3]
+# print(mylist)
+# a = input("write smthng: ")
+# # print(a+"Hello")
+# NAME = "Serhii"
+# a = 10
+# # print(str(10.55)+str(1.0))
+# b = "Hello, world"
+# print(b[0])
+# print(b[-1])
+# # print(10==9)
+# a = "tenet"
+# print(a[0]==a[-1])
+# if a[0]==a[-1]:
+#     print("Well done")
+# else: 
+#     print("Check again")
